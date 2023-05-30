65cca5e58af97983c179e67ef1704de1001b0dc4
Stephane Nicoll <snicoll@vmware.com>
2023-05-30 15:16:26 +0200

    Upgrade to Mojo Parent 74

--- src/it/xinclude-xsl/verify.groovy
+++ src/it/xinclude-xsl/verify.groovy
@@ -1,11 +1,11 @@
-import org.codehaus.plexus.util.FileUtils
+import groovy.xml.XmlSlurper
 
 File xmlFile = new File(basedir, "/target/generated-resources/xml/xslt/book.xml")
 assert xmlFile.isFile()
 
 def book = new XmlSlurper().parse(xmlFile)
 
-assert book instanceof groovy.util.slurpersupport.GPathResult
+assert book instanceof groovy.xml.slurpersupport.GPathResult
 
 assert book.chapter.section == 'Hello World'
 
